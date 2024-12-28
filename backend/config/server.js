40a09ac337a395ecfc55d4876de40baf856c95ba
Matteo Migliarese <migliaresematteo@gmail.com>
2024-12-28 23:07:35 +0100

    up

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      'Kt6RNz9wHJLZX2vM4YqS8PcF3gDbWeUj',
+      'mN7xA5hQ9kVfE2cP8yBtL4wD6sRjGvXu',
+      'bT5nC8mK2xR7vW4pY9fH6gJ3sLdAeUqZ',
+      'cX6vB9nM4kR8tW2hL5jF7gA3sPdEuYqZ'
+    ],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
